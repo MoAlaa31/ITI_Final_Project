1d54792b0f9178074f4c3175c4ceb7aeafc3ff9b
iti_Mohamed_Gad <mohamed.gad.eng@gmail.com>
2026-02-25 00:42:32 +0200

    create service Request endpoint

---
+++ ITI_Project.Api/DTO/Requests/ServiceRequestDTO.cs
@@ -0,0 +1,18 @@
+using ITI_Project.Core.Enums;
+using ITI_Project.Core.Models.Location;
+
+namespace ITI_Project.Api.DTO.Requests
+{
+    public class ServiceRequestDTO
+    {
+        public int Id { get; set; }
+        public RequestStatus RequestStatus { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal? FinalPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? PreferredTime { get; set; }
+        public int ClientId { get; set; }
+        public int? ProviderId { get; set; }
+        public ServiceRequestLocation? ServiceRequestLocation { get; set; }
+    }
+}
